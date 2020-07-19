4c74eb643c8abcba6e9a7686092368d9c64a49e4
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-07-19 23:20:15 +0200

    Some minimal style changes

--- source/shape.cpp
+++ source/shape.cpp
@@ -1,7 +1,7 @@
-#include "shape.hpp"
-#include "color.hpp"
 #include <iostream>
 #include <string>
+#include "shape.hpp"
+#include "color.hpp"
 
 Shape::Shape():
 name_{"Nameless shape"},
@@ -12,7 +12,7 @@ color_{Color{30.0f, 30.0f, 30.0f}}
 
 
 Shape::Shape(std::string name, Color color):
-name_{name},
+name_{"Nameless shape"},
 color_{color}
 {
 
