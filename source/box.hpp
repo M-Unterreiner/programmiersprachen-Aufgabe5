9fe1eaceb0f81c83096ebc0f36be2204aa5cf166
Martin Unterreiner <martin.unterreiner@posteo.de>
2020-07-19 23:19:17 +0200

    Added box.hpp with constructors

---
+++ source/box.hpp
@@ -0,0 +1,21 @@
+#ifndef BOX_HPP
+#define BOX_HPP
+
+#include <glm/vec3.hpp>
+#include "shape.hpp"
+#include "color.hpp"
+
+class Box: public Shape
+{
+  public:
+  Box();
+  Box(glm::vec3 const& minimum, glm::vec3 const& maximum);
+  Box(glm::vec3 const& minimum, glm::vec3 const& maximum, std::string name, Color color);
+
+  private:
+  glm::vec3 minimum_;
+  glm::vec3 maximum_;
+
+};
+
+#endif
